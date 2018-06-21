676c42c97fc018a8ef5f57ba2fde573f28945154
rebeccapotje <rebecca.potje@gmail.com>
2018-06-21 19:10:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file.
+
+
+
+//let lowerCaseStudentNames = ['Adele', 'Beyonce', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
+
+function other (student) {
+  return student.toLowerCase();
+}
+
+function lowerCaseStudentNames (list) {
+return list.map(other)
+}
+
+function splitName(name) {
+  const studentFirst = name.split(" ")[0];
+  const studentLast = name.split(" ")[1];
+  
+  return { firstName : studentFirst, lastName : studentLast};
+}
+
+function nameToAttributes(list) {
+  return list.map(splitName);
+}
+
+function attributesToPhrase(name) {
+  const
+}
